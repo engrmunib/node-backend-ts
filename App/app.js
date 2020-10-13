c7aa0da9ec471df71086d5a5afdb0cf60cb4273d
farrukh_taqveem <farrukh@cognitivehealthintl.com>
2020-10-13 22:20:03 +0500

    App Changes

--- App/app.js
+++ App/app.js
@@ -7,17 +7,18 @@ const app = express();
 
 app.use(cors());
 app.options("*", cors());
+
 app.use(bodyParser.json());
+
 app.use(compression());
+
 app.use("/user", userRoute);
 app.all("*", (req, res, next) => {
-
- 
-  res.status(200).send(
-    "<h1>Hey This is Server, call APIs man. InSource, Lets's Connect</h1>"
+  res.status(404).send(
+    {
+      message:`Can't find ${req.originalUrl} on this server!`
+    }
   );
-  //next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-//app.use(globalErrorHandler);
 module.exports = app;

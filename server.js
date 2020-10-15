4a7905c6554c7e7a910e087006bda142c6fba612
farrukh_taqveem <farrukh@cognitivehealthintl.com>
2020-10-15 19:08:33 +0500

    tesing

--- server.js
+++ server.js
@@ -18,3 +18,10 @@ const sequelize = new Sequelize(DATABASE, DB_USER, DB_PASS, {
 }).then(() =>{
   console.log(`DB CONNECTION SUCCESSFUL with ${DATABASE}`)
 });
+
+// try {
+//   await sequelize.authenticate();
+//   console.log('Connection has been established successfully.');
+// } catch (error) {
+//   console.error('Unable to connect to the database:', error);
+// }

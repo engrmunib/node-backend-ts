c5f58008189394c86d1d73436fc5b048a7433365
farrukh_taqveem <farrukh@cognitivehealthintl.com>
2020-10-15 21:30:18 +0500

    setupServer.js

--- setupServer.js
+++ setupServer.js
@@ -1,5 +1,11 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: ".env" });
 
-const app = require("./App/app");
 const db =  require("./App/db")
+
+const migrateModelChanges = async()=>{
+    console.log('Migrating Model Changes to Database')
+    await db.sync({ force: true });
+    console.log('Success')
+}
+migrateModelChanges();

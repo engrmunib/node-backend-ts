df51e291ceeca247e6523ae0e6de42e007ee66ba
farrukh_taqveem <farrukh@cognitivehealthintl.com>
2020-10-15 20:31:27 +0500

    DB Created

--- App/db.js
+++ App/db.js
@@ -1,20 +1,20 @@
 const { Sequelize } = require('sequelize');
 
-const {DATABASE, DB_USER, DB_PASS, DB_HOST} = process.env;
+const {DATABASE, DB_USER, DB_PASS, DB_HOST, DB_PORT} = process.env;
 
 const sequelize= new Sequelize(DATABASE, DB_USER, DB_PASS, {
     host: DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: DB_PORT
   })
 
 sequelize.connectionTest = async()=>{
     try {
         await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
+        console.log('DB Connection has been established successfully.');
       } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
 }
-
 
 module.exports = sequelize;
